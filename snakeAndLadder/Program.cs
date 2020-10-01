e0281cdac5df7ef01d07af69baa7182276a0bde6
N1vas <ggj.nivas.18@gmail.com>
2020-10-01 12:30:08 +0530

    [N1vas] Refactor : UC1_Game PLayed with Single Player at Start Position https://github.com/N1vas/SnakeAndLadderSimulation.git

---
+++ snakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace snakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Simulation(Single Player)");
+            int playerOnePosition = 0;
+            Console.WriteLine("Position of Player 1 is at "+playerOnePosition);
+        }
+    }
+}
