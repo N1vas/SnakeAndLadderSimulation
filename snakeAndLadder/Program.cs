7d6af9a706092249583dfdf41c4fa577b1414f12
N1vas <ggj.nivas.18@gmail.com>
2020-10-01 12:41:14 +0530

    [N1vas] Refactor : UC2_Player Rolls the Die

--- snakeAndLadder/Program.cs
+++ snakeAndLadder/Program.cs
@@ -7,6 +7,10 @@ namespace snakeAndLadder
         {
             Console.WriteLine("Welcome to Snake and Ladder Simulation(Single Player)");
             int playerOnePosition = 0;
+            Random random = new Random();
+            int dieRoll;
+            dieRoll = random.Next(1, 7);
+            playerOnePosition += dieRoll;
             Console.WriteLine("Position of Player 1 is at "+playerOnePosition);
         }
     }
