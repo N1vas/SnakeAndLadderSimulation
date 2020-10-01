36de4a2dd0d0011c068a66da47d93b678af80079
N1vas <ggj.nivas.18@gmail.com>
2020-10-01 14:54:14 +0530

    UC3 check for noplay, ladder and snake

--- snakeAndLadder/Program.cs
+++ snakeAndLadder/Program.cs
@@ -5,12 +5,29 @@ namespace snakeAndLadder
     {
         static void Main(string[] args)
         {
+            const int NO_PLAY = 1;
+            const int LADDER = 2;
+            const int SNAKE = 3;
             Console.WriteLine("Welcome to Snake and Ladder Simulation(Single Player)");
             int playerOnePosition = 0;
             Random random = new Random();
             int dieRoll;
             dieRoll = random.Next(1, 7);
-            playerOnePosition += dieRoll;
+            int check = random.Next(1,4);
+            switch(check) 
+            {
+                case NO_PLAY:
+                    playerOnePosition = playerOnePosition + 0;
+                    break;
+                case LADDER:
+                    playerOnePosition = playerOnePosition + dieRoll;
+                    break;
+                case SNAKE:
+                    playerOnePosition = playerOnePosition - dieRoll ;
+                    if (playerOnePosition < 0)
+                        playerOnePosition = 0;
+                    break;
+            }
             Console.WriteLine("Position of Player 1 is at "+playerOnePosition);
         }
     }
