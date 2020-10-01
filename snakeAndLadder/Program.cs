c202bcc75229dbe3b158c870cf07f63430c70161
N1vas <ggj.nivas.18@gmail.com>
2020-10-01 16:12:37 +0530

    [N1vas] Refactor : UC4 Repeat Die Position till 100

--- snakeAndLadder/Program.cs
+++ snakeAndLadder/Program.cs
@@ -12,23 +12,26 @@ namespace snakeAndLadder
             int playerOnePosition = 0;
             Random random = new Random();
             int dieRoll;
-            dieRoll = random.Next(1, 7);
-            int check = random.Next(1,4);
-            switch(check) 
+            while (playerOnePosition < 100)
             {
-                case NO_PLAY:
-                    playerOnePosition = playerOnePosition + 0;
-                    break;
-                case LADDER:
-                    playerOnePosition = playerOnePosition + dieRoll;
-                    break;
-                case SNAKE:
-                    playerOnePosition = playerOnePosition - dieRoll ;
-                    if (playerOnePosition < 0)
-                        playerOnePosition = 0;
-                    break;
-            }
-            Console.WriteLine("Position of Player 1 is at "+playerOnePosition);
+                dieRoll = random.Next(1, 7);
+                int check = random.Next(1, 4);
+                switch (check)
+                {
+                    case NO_PLAY:
+                        playerOnePosition += 0;
+                        break;
+                    case LADDER:
+                        playerOnePosition += dieRoll;
+                        break;
+                    case SNAKE:
+                        playerOnePosition -= dieRoll;
+                        if (playerOnePosition < 0)
+                            playerOnePosition = 0;
+                        break;
+                }
+                Console.WriteLine("Position of Player 1 is at " + playerOnePosition);
+            }   
         }
     }
 }
